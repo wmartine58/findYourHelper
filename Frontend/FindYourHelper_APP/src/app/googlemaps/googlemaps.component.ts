9d1608bc76197d312954dee80db41916ac3afb92
wmartine58 <wellinmart58@gmail.com>
2024-05-02 23:40:01 -0500

    creating googlemaps component and service

---
+++ Frontend/FindYourHelper_APP/src/app/googlemaps/googlemaps.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-googlemaps',
+  templateUrl: './googlemaps.component.html',
+  styleUrls: ['./googlemaps.component.scss'],
+})
+export class GooglemapsComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
